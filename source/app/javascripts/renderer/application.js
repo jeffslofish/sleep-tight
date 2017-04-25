adc41554f19f93fb077314a9781fdc2b3fd18d07
Alex Pendleton <alexcpendleton@gmail.com>
2017-04-24 22:10:27 -0700

    Non-working attempt at photon-kit. Trying to resolve build issue

--- source/app/javascripts/renderer/application.js
+++ source/app/javascripts/renderer/application.js
@@ -1,10 +1,15 @@
-require('electron').ipcRenderer.on('loaded' , function(event, data) {
-  document.getElementById('title').innerHTML = data.appName + ' App';
-  document.getElementById('details').innerHTML = 'built with Electron v' + data.electronVersion;
-  document.getElementById('versions').innerHTML = 'running on Node v' + data.nodeVersion + ' and Chromium v' + data.chromiumVersion;
-});
+import React from "react";
+import ReactDom from "react-dom";
+import { Window, Content, PaneGroup ,Pane } from "react-photonkit";
 
-
-require('electron').ipcRenderer.on('update-remaining-time', function(event, data) {
-  console.log("update-remaining-time called", arguments);
-});
+ReactDom.render(
+  <Window>
+    <Content>
+      <PaneGroup>
+        <Pane className="padded-more">
+          Hello, react-photonkit!!!
+        </Pane>
+      </PaneGroup>
+    </Content>
+  </Window>
+  , document.querySelector("#main"));
